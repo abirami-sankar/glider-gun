3b38e1aea6e64ba5e62708d269cda6a36cef8219
abirami-sankar <abirami.sankar1410@gmail.com>
2021-10-23 15:10:22 +0530

    gol v1.1

--- life-react/src/components/World.js
+++ life-react/src/components/World.js
@@ -4,7 +4,7 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 import * as dat from 'three/examples/jsm/libs/dat.gui.module';
 
 
-const size = 25;
+const size = 45;
 //const density = 1000;
 
 class World extends React.Component {
@@ -15,9 +15,7 @@ class World extends React.Component {
         // including the standard animate function in three.js which is bound
         // to class since we cant access this.scene in outside function (private)
         this.start = this.start.bind(this);
-        this.drawAxes = this.drawAxes.bind(this);
-        this.generateCubes = this.generateCubes.bind(this);
-        this.updateCubes = this.updateCubes.bind(this);
+        this.drawCubes = this.drawCubes.bind(this);
         this.nextGeneration = this.nextGeneration.bind(this);
         this.populateWorld = this.populateWorld.bind(this);
         this.resetWorld = this.resetWorld.bind(this);
@@ -39,11 +37,7 @@ class World extends React.Component {
 
         // co-ordinate axis just to provide some reference
         //const line_material = new THREE.LineBasicMaterial({ color: 0x0000ff });
-       // this.drawAxes(line_material);
-
-       // add empty cubes to the scene
-        this.generateCubes();
-        
+       // this.drawAxes(line_material); 
 
         // camera
         const camera = new THREE.PerspectiveCamera( 75, 
@@ -96,14 +90,17 @@ class World extends React.Component {
 
         this.mount.appendChild( renderer.domElement );
     }
-
-    setup() {
-
-    }
     
     start() {
-        this.cubeIndex = make3DArray(this.densityRef);
-        this.updateCubes();
+        //this.cubeIndex = make3DArray(this.densityRef);
+        //this.updateCubes();
+
+        this.cubeIndex = make3DArray(this.densityRef.value);
+        
+        this.drawCubes();
+
+        console.log("density: ",this.densityRef.value)
+        console.log("rule: ",this.ruleRef.value)
 
         // start the animation
         if (!this.frameId) {
@@ -112,53 +109,21 @@ class World extends React.Component {
         //this.nextGeneration();
     }
 
-    drawAxes(material) {
-        const pointsX = [];
-        pointsX.push(new THREE.Vector3(-100, 0, 0));
-        pointsX.push(new THREE.Vector3(0, 0, 0));
-        pointsX.push(new THREE.Vector3(100, 0, 0));
-
-        const geometryX = new THREE.BufferGeometry().setFromPoints(pointsX);
-        const lineX = new THREE.Line(geometryX, material);
-        this.scene.add(lineX);
-
-        const pointsY = [];
-        pointsY.push(new THREE.Vector3(0, -100, 0));
-        pointsY.push(new THREE.Vector3(0, 0, 0));
-        pointsY.push(new THREE.Vector3(0, 100, 0));
-
-        const geometryY = new THREE.BufferGeometry().setFromPoints(pointsY);
-        const lineY = new THREE.Line(geometryY, material);
-        this.scene.add(lineY);
-    }
-
-    generateCubes(){
+    drawCubes(){
         const geometry = new THREE.BoxGeometry(1,1,1);
-        
-        for(let i = 0; i < size; i++){
-            for(let j = 0; j < size; j++){
-                for(let k = 0; k < size; k++){
-                    const material = new THREE.MeshStandardMaterial( { color: 0xff2399 } );
-                    material.transparent = true;
-                    material.opacity = 0;
-                    material.roughness = 0;
-                    const cube = new THREE.Mesh( geometry, material );
-                    cube.position.set(i,j,k);
-                    cube.is_ob = true;
-                    this.scene.add( cube );
-                }
-            }
-        }
-    }
+        const material = new THREE.MeshStandardMaterial( { color: 0xff2399 } );
+        material.transparent = true;
+        material.opacity = 0.5;
+        material.roughness = 0;
 
-    updateCubes(){   
         for(let i = 0; i < size; i++){
             for(let j = 0; j < size; j++){
                 for(let k = 0; k < size; k++){
                     if (this.cubeIndex[i][j][k].state === 1) {
-                        const cube = this.scene.getObjectById(this.cubeIndex[i][j][k].id)
-                        console.log(cube);
-                        cube.material.opacity = 0.5;
+                        const cube = new THREE.Mesh( geometry, material );
+                        cube.position.set(i,j,k);
+                        cube.is_ob = true;
+                        this.scene.add( cube );
                     }
                 }
             }
@@ -176,12 +141,40 @@ class World extends React.Component {
 
                     // load in ruleset based on dropdown
                     let neighbors
-                    switch(this.ruleRef){
+                    switch(this.ruleRef.value){
                         case "Moore":
-                            neighbors = checkNeighborsM(this.cubeIndex, k, j, i)
+                            neighbors = checkNeighborsM(this.cubeIndex, k, j, i);
+                            if (cell.state === 0) {
+                                if (neighbors === 5) {
+                                    newGen[i][j][k].state = 1
+                                } else {
+                                    newGen[i][j][k].state = 0
+                                }
+                            }
+                            else {
+                                if (neighbors === 5 || neighbors === 6) {
+                                    newGen[i][j][k].state = 1
+                                } else if(neighbors >= 7 || neighbors <= 4) {
+                                    newGen[i][j][k].state = 0 
+                                }
+                            }
                             break;
                         case "Neumann":
                             neighbors = checkNeighborsN(this.cubeIndex, k, j, i)
+                            if (cell.state === 0) {
+                                if (neighbors === 3) {
+                                    newGen[i][j][k].state = 1
+                                } else {
+                                    newGen[i][j][k].state = 0
+                                }
+                            }
+                            else {
+                                if (neighbors === 2 || neighbors === 3) {
+                                    newGen[i][j][k].state = 1
+                                } else if(neighbors >= 4 || neighbors <= 1) {
+                                    newGen[i][j][k].state = 0 
+                                }
+                            }
                             break;
                         default:
                             neighbors = checkNeighborsM(this.cubeIndex, k, j, i)
@@ -190,17 +183,6 @@ class World extends React.Component {
                     
                     //console.log("neighbors: ", neighbors)
 
-                    if (cell.state === 0) {
-                        if (neighbors === 3) {
-                            newGen[i][j][k].state = 1
-                        }
-                    } else {
-                        if (neighbors === 3 || neighbors === 2) {
-                            newGen[i][j][k].state = 1
-                        } else if(neighbors >= 4 || neighbors <= 1) {
-                            newGen[i][j][k].state = 0 
-                        }
-                    }
                 }
             }
         }
@@ -208,17 +190,22 @@ class World extends React.Component {
         //console.log(newGen);
 
         this.cubeIndex = newGen;
-        this.updateCubes();
+        //this.updateCubes();
+        this.populateWorld();
     }
 
     populateWorld() {
-        this.updateCubes();
+        this.clearScene();
+        this.drawCubes();
     }
 
     resetWorld() {
-        this.cubeIndex = make3DArray(this.densityRef);
+        this.clearScene();
+
+        this.cubeIndex = make3DArray(this.densityRef.value);
+        this.drawCubes();
     
-        this.updateCubes();
+        //this.updateCubes();
     }
 
     clearScene() {
@@ -244,13 +231,13 @@ class World extends React.Component {
         return (
             <div>
                 <div>
-                    <select ref = {ref => (this.ruleRef = ref.value)} id="rulesets" name="rulesets" defaultValue = "select">
+                    <select ref = {ref => (this.ruleRef = ref)} id="rulesets" name="rulesets" defaultValue = "select" required>
                         <option value="select" disabled>Select Ruleset</option>
                         <option value="Moore">GOL- Moore neighborhood</option>
                         <option value="Neumann">GOL- Neumann neighborhood</option>
                     </select>
 
-                    <input ref = {ref => (this.densityRef = ref)} id = "density" type="number" placeholder="Set Density" />
+                    <input ref = {ref => (this.densityRef = ref)} id = "density" type="number" placeholder="Set Density" required />
                     <button id = "start" onClick={this.start}>Start</button>
                 </div>
                 <button id = "reset" onClick={this.resetWorld}>Reset World</button>
@@ -284,7 +271,7 @@ function make3DArray(density) {
     for (let i = Math.floor(size/4); i < Math.floor(3*size/4); i++) {
         for (let j = Math.floor(size/4); j < Math.floor(3*size/4); j++) {
             for (let k = Math.floor(size/4); k < Math.floor(3*size/4); k++) {
-                arr[i][j][k].state = Math.floor(Math.random() * 2) === 1 ? 1: 0;
+                arr[i][j][k].state = Math.floor(Math.random() * 3) === 1 ? 1: 0;
                 if (arr[i][j][k].state === 1) {
                     cubes++;
                     if (cubes > density) {
